0d1bdd4d2865b7367d304b165eec113367550418
ecb7gr <143363131+ecb7gr@users.noreply.github.com>
2025-03-26 20:59:32 -0400

    Initial commit

---
+++ transition.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+@onready var color_rect = $ColorRect
+@onready var anim_player = $AnimationPlayer
+signal on_transition_finished
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	color_rect.visible = false
+	anim_player.animation_finished.connect(_on_animation_finished)
+	pass # Replace with function body.
+func transition():
+	color_rect.visible = true
+	anim_player.play("fade_to_black")
+func _on_animation_finished(anim_name):
+		if anim_name == "fade_to_black":
+			anim_player.play("fade_from_black")
+			on_transition_finished.emit()
+		elif anim_name == "fade_from_black":
+			color_rect.visible = false
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
