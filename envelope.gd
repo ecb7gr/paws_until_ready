ab1249b54be50f54312e2df0be08a999c9d8d41d
ecb7gr <143363131+ecb7gr@users.noreply.github.com>
2025-03-30 21:13:46 -0400

    this is pure chaos

--- envelope.gd
+++ envelope.gd
@@ -1,32 +1,23 @@
 extends Area2D
-var in_body = false
+var processing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	input_pickable = true
 	monitoring = true
+	set_process_input(true)
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-
 	pass
-
-
-
-func _on_area_entered(area: Area2D) -> void:
-	in_body = true
-	pass # Replace with function body.
-
-
-
-
-func _on_area_exited(area: Area2D) -> void:
-	in_body=false
-	pass # Replace with function body.
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed):
-		get_tree().change_scene_to_file("res://board_view.tscn")
+		Transition.transition()
+		await Transition.on_transition_finished
+		get_tree().change_scene_to_file("res://intro_cut_scene.tscn")
 	pass # Replace with function body.
+ # Replace with function body.
