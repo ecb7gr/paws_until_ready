520578441ce28dcbfbb6c0fa5927246bcca9c869
ecb7gr <143363131+ecb7gr@users.noreply.github.com>
2025-04-29 10:01:03 -0400

    more styff for third scene

--- a_very_sad_dog.gd
+++ a_very_sad_dog.gd
@@ -1,8 +1,11 @@
-extends Node2D
+extends Area2D
 @onready var bub = $AnimatedSprite2D/Sprite2D
 @onready var dis = $AnimatedSprite2D/Sprite2D/AnimatedSprite2D
+@onready var rel = $AnimatedSprite2D/Sprite2D/Sprite2D
+signal close_enough_to_dog
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	monitoring = true
 	
 	pass # Replace with function body.
 func hide_bub():
@@ -11,7 +14,20 @@ func show_bub():
 	bub.show()
 func set_distress():
 	dis.show()
+	dis.show()
+func set_not_distressed():
+	dis.hide()
+func set_relax():
+	rel.show()
+func set_un_relax():
+	rel.hide()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body) -> void:
+	print("trigger")
+	close_enough_to_dog.emit()
+	pass # Replace with function body.
--- third_game_scene.gd
+++ third_game_scene.gd
@@ -5,10 +5,13 @@ extends Node2D
 @onready var cut_cam = $Camera2D
 @onready var anim = $Camera2D/AnimationPlayer
 @onready var sad_dog = $Node2D
+@onready var coll_for_scene = $CharacterBody2D/CollisionShape2D2
+@onready var timer = $Timer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ghost.set_inactive()
 	sad_dog.hide_bub()
+	sad_dog.get_child(1).disabled = true
 	cut_cam.make_current() #CHATGPT4 helped me find this method
 	energy.increment_energy_value()
 	energy.increment_energy_value()
@@ -27,12 +30,55 @@ func _ready() -> void:
 	TextBox.queue_text("Well, I guess I better go say hi.")
 	TextBox.show()
 	TextBox.display_text()
+	await TextBox.on_display_finished
 	camera.make_current()
 	ghost.set_active()
 	sad_dog.show_bub()
+	sad_dog.set_un_relax()
+	sad_dog.get_child(1).disabled = false
+	sad_dog.close_enough_to_dog.connect(_test)
+	await TextBox.on_display_finished
+	
+	
 	pass # Replace with function body.
 
-
+func _test():
+	TextBox.queue_text("Hey, dude,")
+	TextBox.queue_text("You alright?")
+	TextBox.display_text()
+	await TextBox.on_display_finished
+	sad_dog.get_child(0).play()
+	timer.start()
+	await timer.timeout
+	ghost.set_inactive()
+	TextBox.queue_text("Oh my god,")
+	TextBox.queue_text("I think I would die a second time for you.")
+	TextBox.display_text()
+	await TextBox.on_display_finished
+	timer.start()
+	await timer.timeout
+	sad_dog.get_child(0).stop()
+	TextBox.queue_text("That being said...")
+	TextBox.queue_text("I genuinely have no idea what you're trying to say")
+	TextBox.display_text()
+	await TextBox.on_display_finished
+	sad_dog.set_not_distressed()
+	sad_dog.set_relax()
+	timer.start()
+	await timer.timeout
+	TextBox.queue_text("Thanks, that doesn't help")
+	TextBox.display_text()
+	await TextBox.on_display_finished
+	sad_dog.set_distress()
+	sad_dog.set_un_relax()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_collision_shape_2d_2_tree_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_2d_close_enough_to_dog() -> void:
+	pass # Replace with function body.
